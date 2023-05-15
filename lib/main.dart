9a518788286b6d6e0e654a74406dd8db8fe2b348
Gabriel <dedito182@gmail.com>
2023-05-15 11:46:03 -0300

    creating simple app

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,149 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(const AppWidget(
-    title: 'Fast and Furious'
-  ));
+  runApp(MyApp());
 }
 
-class AppWidget extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter layout demo',
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Flutter layout demo'),
+            ),
+            body: Column(children: [titleSection])));
+  }
 
-  final String title;
+  Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: const Text('Lake riverdale south',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ))),
+                Text('Ireland Galway',
+                    style: TextStyle(color: Colors.grey[500]))
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('4.1'),
+        ],
+      ));
 
-  const AppWidget({Key? key, required this.title}) : super(key: key);
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+/* Stateful widget is a type of widget that can maintain mutable state. This means
+* that the properties or configs can change over time.  */
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        title,
-        textDirection: TextDirection.ltr,
-      )
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited
+                ? const Icon(Icons.star)
+                : const Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
     );
   }
+}
 
+/* Stateless widget is a type of widget that doesn't have any mutable state.
+It'' immutable, meaning its properties or configs
+cannot change once it is instantialied. */
+
+class Movies extends StatelessWidget {
+  const Movies({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello World'),
+        ),
+        body: const Center(
+          child: Text(
+            'List of movies!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
 }
